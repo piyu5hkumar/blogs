52a2d1c41ef10b9387855363b996f60c0d3dd73b
Piyush Kumar <piyushkumar031997@gmail.com>
2024-05-30 01:02:56 +0530

    Blog Model 1. added title_slug 2. pre_save signal for set_title_slug

--- blogging/models.py
+++ blogging/models.py
@@ -3,14 +3,28 @@ from utils.abstract_models import BaseModel
 from blog_users.models import User
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+
 
 
 class Blog(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
+    title_slug = models.SlugField(null=True, unique=True, blank=True)
     content = RichTextUploadingField()
     total_likes = models.PositiveIntegerField(default=0)
     total_comments = models.PositiveIntegerField(default=0)
+
+
+    def __str__(self):
+        return self.title
+
+
+@receiver(pre_save, sender=Blog)
+def set_title_slug(sender, instance: Blog, **kwargs):
+    instance.title_slug = slugify(instance.title)
 
 
 class Comment(BaseModel):
@@ -28,6 +42,9 @@ class Like(BaseModel):
 class Topic(BaseModel):
     name = models.CharField(max_length=100)
     total_blogs = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 #### Mappings ###
